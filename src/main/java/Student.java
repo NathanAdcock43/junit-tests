d831474edef11419f4ff9972e09509fb1dd378ad
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-14 13:54:34 -0500

    feat: new branch

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+
 public class Student {
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+    // returns the student's id
+    public long getId(){
+        return Student.id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return Student.name;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return ;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        return ;
+    }
 
 }

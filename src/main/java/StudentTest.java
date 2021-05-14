3023492f3827fe320250b7f8026a31a856984526
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-14 12:04:55 -0500

    Project Setup: Student Class and StudentTest class, Methods incomplete

---
+++ src/main/java/StudentTest.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class StudentTest {
+    // returns the student's id
+    public long getId(){
+        return ;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return ;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return ;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        return ;
+    }
+}
